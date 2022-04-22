8e8c722e2f3ea4c6e7157f7cecb3bee13ff19d3e
Jonas Scholz <js@pep.security>
2022-04-22 09:33:16 +0200

    loest dependency injection fehler
    Allerdings wird dabei die abstrakte klasse verworfen.
     Evtl kann man das noch verbessern.

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { SearchModule } from '@red-probeaufgabe/search';
+import { SearchFacadeService, SearchModule } from '@red-probeaufgabe/search';
 import { UiModule } from '@red-probeaufgabe/ui';
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [CommonModule, UiModule, SearchModule, DashboardRoutingModule],
   exports: [DashboardComponent],
+  providers: [SearchFacadeService]
 })
 export class DashboardModule {}
