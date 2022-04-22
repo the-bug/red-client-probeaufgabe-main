c3436edecbc4936b2bbd942b2cbe03616669e19d
Jonas Scholz <js@pep.security>
2022-04-22 11:23:52 +0200

    Fuegt ladeanimation bei eingabe neuer daten hinzu

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -40,6 +40,7 @@ export class DashboardComponent {
   }
   
   searchFormInputChange(formValues: SearchFormInput) {
+    this.isLoading = true;
     console.log(formValues)
     this.formValues = formValues;
 
