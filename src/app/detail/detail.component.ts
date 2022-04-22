4ee4a72d3baf0ea89e63e7cd8e090d3670c70d24
Jonas Scholz <js@pep.security>
2022-04-22 12:48:21 +0200

    Fuegt auswertung der detail hinzu mit rudimentaerer ansicht
    Da muss allerdings noch ein bisschen das UI poliert werden

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AbstractSearchFacadeService } from '@red-probeaufgabe/search';
 
 @Component({
   selector: 'app-detail',
@@ -11,13 +12,25 @@ export class DetailComponent {
   id: string;
   type: string;
   private sub: any;
+  data;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private searchFacade: AbstractSearchFacadeService) { }
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
-      this.id = params['id']; 
-      this.type = params['type']; 
+      this.id = params['id'];
+      this.type = params['type'];
+
+      // TODO Fehlerbehandlung machen, wenn daten nicht gefunden wurden...
+      if (this.type === 'Patient') {
+        this.searchFacade.findPatientById(this.id).subscribe(data => { this.data = data });
+      }
+      else if (this.type === 'Practitioner') {
+        this.searchFacade.findPractitionerById(this.id).subscribe(data => { this.data = data });
+      } else {
+        // TODO bessere fehlerbehandlung. evtl mut route gurds.
+        console.log("falscher pfad paramter -> Programmierer ist schuld...")
+      }
     });
   }
 
